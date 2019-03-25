9b5acd3fcff36405f41e0979ada1a8bba953a34e
Alberto Belotti <albertopuntobelotti@gmail.com>
2019-03-25 11:10:20 +0100

    Update keywords.php

--- keywords.php
+++ keywords.php
@@ -3,18 +3,19 @@ $file = file("keywords.csv");
 $n = $z = 0;
 $chars = array("\r\n", '\\n', '\\r', "\n", "\r", "\t", "\0", "\x0B");
 foreach ($file as $row) {
-	$n++;
-	if ($n < 4) {
-		continue;
-	}
-	$exp = explode(",",$row);
-	$firstColumn = str_replace($chars,"",trim($exp[0]));
-	$totale = str_replace($chars,"",trim($exp[2]));
-	$punteggiatura = [".",",",";"];
-	$vocali = ["a","e","o","i"];
+    $n++;
+    if ($n < 4) {
+        continue;
+    }
+    $exp = explode(",",$row);
+    $firstColumn = str_replace($chars,"",trim($exp[0]));
+    $totale = str_replace($chars,"",trim($exp[2]));
+    $punteggiatura = [".",",",";"];
+    $vocali = ["a","e","o","i"];
 
-	if (stripos($firstColumn, "premium") !== false) {
-		if($totale > 1500)
-			echo $firstColumn . " | ";
+    if (stripos($firstColumn, "premium") !== false) {
+	if($totale > 1500) {
+	    echo $firstColumn . " | ";
 	}
+    }
 }
